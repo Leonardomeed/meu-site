ba70219bd5b10833ff0789e9f0a78bf22b652586
leonardomeed <lmsantosqi@gmail.com>
2025-05-16 19:52:09 -0300

    first commit

---
+++ Script.js
@@ -0,0 +1,4 @@
+document.getElementById('mensagemBotao').addEventListener('click', function() {
+    const mensagemDiv = document.getElementById('mensagem');
+    mensagemDiv.innerHTML = '<p>Seja bem-vindo a minha página. Lembre-se você é incrível! 🚀</p>';
+});
